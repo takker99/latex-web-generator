07e4e87cd4066930b7adae359ec2ec063b60aa6c
takker99 <37929109+takker99@users.noreply.github.com>
2020-08-15 03:06:56 +0900

    :alembic: mathjaxで再び出力してみる

--- api/latex.ts
+++ api/latex.ts
@@ -1,44 +1,42 @@
 import {NowRequest, NowResponse} from '@vercel/node';
-/* import {mjAPI} from 'mathjax-node'; */
+import mjAPI from 'mathjax-node';
 
 export default (request: NowRequest, response: NowResponse): void => {
-    // a simple TeX-input example
-    /* mjAPI.config({ */
-    /*     MathJax: { */
-    /*         // traditional MathJax configuration */
-    /*     } */
-    /* }); */
-    /* mjAPI.start(); */
+// a simple TeX-input example
+    mjAPI.config({
+        MathJax: {
+            // traditional MathJax configuration
+        }
+    });
+    mjAPI.start();
 
-    /* const yourMath = 'E = mc^2'; */
+    const yourMath = 'E = mc^2';
 
-    /* mjAPI.typeset({ */
-    /*     math: yourMath, */
-    /*     format: 'TeX', // or "inline-TeX", "MathML" */
-    /*     svg:true */
-    /* }, function (data:any) { */
-    /*     if (!data.errors) { */
-    /*         response.setHeader('content-type', 'image/svg+xml'); */
-    /*         response.status(200).send(data.svg); */
-    /*     } else { */
-    /*         response.status(200).send('failed'); */
-    /*     } */
-    /*     // will produce: */
-    /*     // <math xmlns="http://www.w3.org/1998/Math/MathML" display="block"> */
-    /*     //   <mi>E</mi> */
-    /*     //   <mo>=</mo> */
-    /*     //   <mi>m</mi> */
-    /*     //   <msup> */
-    /*     //     <mi>c</mi> */
-    /*     //     <mn>2</mn> */
-    /*     //   </msup> */
-    /*     // </math> */
-    /* }); */
-    response.setHeader('content-type', 'image/svg+xml');
-    response.status(200).send(`
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="400" height="100" viewBox="0 0 400 100">
-    <circle cx="50" cy="50" r="40" fill="none" stroke="black" stroke-width="2"></circle>
-    <ellipse cx="200" cy="50" rx="80" ry="40" fill="red" stroke="black" stroke-width="2"></ellipse>
-  </svg>
-                              `);
+    mjAPI.typeset({
+        math: yourMath,
+        format: 'TeX', // "inline-TeX", "MathML"
+        mml:true,
+        svg: true //  svg:true,
+    }, function (data) {
+        if (!data.errors) {
+            const output = '<span aria-hidden=\'true\'>' + data.html + '</span><span class=\'hidden\'> ' + data.mml + '</span>';
+            // where class=hidden might include
+            // position:absolute!important, top: 0, left: 0;
+            // clip: rect(1px 1px 1px 1px) : rect(1px, 1px, 1px, 1px));
+            // padding: 1px 0 0 0!important;
+            // border: 0!important;
+            // height: 1px!important;
+            // width: 1px!important;
+            // overflow: hidden!important;
+            // display:block!important;
+            // -webkit-touch-callout: none;
+            // -webkit-user-select: none;
+            // -khtml-user-select: none;
+            // -moz-user-select: none;
+            // -ms-user-select: none;
+            // user-select: none;
+            response.setHeader('content-type', 'image/svg+xml');
+            response.status(200).send(output);
+        }
+    });
 };
